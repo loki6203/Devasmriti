0966f9c26e83b2925044d4f2627c5b4b37177b5b
venkat rao <venkat@vibhotech.com>
2023-10-31 18:39:55 +0530

    Access key issue fixed

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -23,19 +23,15 @@ class OrderController extends Controller
 {
 	public $succ = 200;
     public $err  = 202;
-    // public $working_key = 'E4ED6ECE68033389BD648618D4B83215';    //Shared by CCAVENUES
-    // public $access_code = 'AVXC97KH69AS87CXSA';                  //Shared by CCAVENUES
-    // public $merchant_id = '2742697';                             //Shared by CCAVENUES
-
-    // public $working_key = '664AB39BBF9119447E372FEF436DCA7D';       //Shared by CCAVENUES
-    // public $access_code = 'AVHN05KJ30CF33NHFC';                     //Shared by CCAVENUES
-    // public $merchant_id = '2742697';                                //Shared by CCAVENUES
-    // public $ccurl = ' https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction';
-
-    public $working_key = 'D31380092DBE36A182BC8FEF363A6E95';       //Shared by CCAVENUES
-    public $access_code = 'AVPB26KJ94CL28BPLC';                     //Shared by CCAVENUES
+    public $working_key = '664AB39BBF9119447E372FEF436DCA7D';       //Shared by CCAVENUES
+    public $access_code = 'AVHN05KJ30CF33NHFC';                     //Shared by CCAVENUES
     public $merchant_id = '2742697';                                //Shared by CCAVENUES
-    public $ccurl = 'https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction';
+    public $ccurl = ' https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction';
+
+    // public $working_key = 'D31380092DBE36A182BC8FEF363A6E95';       //Shared by CCAVENUES
+    // public $access_code = 'AVPB26KJ94CL28BPLC';                     //Shared by CCAVENUES
+    // public $merchant_id = '2742697';                                //Shared by CCAVENUES
+    // public $ccurl = 'https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction';
 
 
     public function __construct(){
--- resources/views/ccavRequestHandler.php
+++ resources/views/ccavRequestHandler.php
@@ -12,9 +12,9 @@ if($isValid>0){
 		$merchant_data.=$key.'='.$value.'&';
 	}
 	$merchant_data='merchant_id=2742697&order_id=1234&currency=INR&amount=1.00&redirect_url=https%3A%2F%2Fapi-backend.devasmriti.com%2Fccavenue%2FresponseHandler&cancel_url=https%3A%2F%2Fapi-backend.devasmriti.com%2Fccavenue%2FresponseHandler&language=EN&billing_name=Peter&billing_address=Santacruz&billing_city=Mumbai&billing_state=MH&billing_zip=400054&billing_country=India&billing_tel=9876543210&billing_email=testing%40domain.com&delivery_name=Sam&delivery_address=Vile+Parle&delivery_city=Mumbai&delivery_state=Maharashtra&delivery_zip=400038&delivery_country=India&delivery_tel=0123456789&merchant_param1=additional+Info.&merchant_param2=additional+Info.&merchant_param3=additional+Info.&merchant_param4=additional+Info.&merchant_param5=additional+Info.&promo_code=&customer_identifier=';
-	echo $merchant_data;
-	echo '<hr>';
-	echo $working_key;
+	// echo $merchant_data;
+	// echo '<hr>';
+	// echo $working_key;
 	$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
 }else{
 	
@@ -25,10 +25,9 @@ if($isValid>0){
 echo "<input type=hidden name=encRequest value=$encrypted_data>";
 echo "<input type=hidden name=access_code value=$access_code>";
 ?>
-<input type="submit"/>
 </form>
 </center>
-<!-- <script language='javascript'>document.redirect.submit();</script> -->
+<script language='javascript'>document.redirect.submit();</script>
 </body>
 </html>
 
