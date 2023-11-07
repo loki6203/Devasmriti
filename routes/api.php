79b5063ebd13d50abed1b6b0a6c94179bbe678a6
venkat rao <venkat@vibhotech.com>
2023-11-07 15:41:35 +0530

    Prasads code setup and adminlogin page enabled

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+URL::forceScheme('https');
 /*
 |--------------------------------------------------------------------------
 | API Routes
