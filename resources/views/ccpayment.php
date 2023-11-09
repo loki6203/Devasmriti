36eb156eaf8b8df8187c8b3977aa55504f2da134
venkat rao <venkat@vibhotech.com>
2023-11-09 19:13:04 +0530

    CC changes

--- resources/views/ccpayment.php
+++ resources/views/ccpayment.php
@@ -8,116 +8,39 @@
 </script>
 </head>
 <body>
-	<form method="post" name="customerData" action="/cc/ccavRequestHandler.php">
-		<table width="40%" height="100" border='1' align="center"><caption><font size="4" color="blue"><b>Integration Kit</b></font></caption></table>
-			<table width="40%" height="100" border='1' align="center">
-				<tr>
-					<td>Parameter Name:</td><td>Parameter Value:</td>
-				</tr>
-				<tr>
-					<td colspan="2"> Compulsory information</td>
-				</tr>
-				<tr>
-					<td>TID	:</td><td><input type="text" name="tid" id="tid" readonly /></td>
-				</tr>
-				<tr>
-					<td>Merchant Id	:</td><td><input type="text" name="merchant_id" value="<?php echo @$respdata['merchant_id'];?>"/></td>
-				</tr>
-				<tr>
-					<td>Order Id	:</td><td><input type="text" name="order_id" value="<?php echo @$respdata['order_id'];?>"/></td>
-				</tr>
-				<tr>
-					<td>Amount	:</td><td><input type="text" name="amount" value="<?php echo @$respdata['amount'];?>"/></td>
-				</tr>
-				<tr>
-					<td>Currency	:</td><td><input type="text" name="currency" value="<?php echo @$respdata['currency'];?>"/></td>
-				</tr>
-				<tr>
-					<td>Redirect URL	:</td><td><input type="text" name="redirect_url" value="<?php echo @$respdata['redirect_url'];?>"/></td>
-				</tr>
-			 	<tr>
-			 		<td>Cancel URL	:</td><td><input type="text" name="cancel_url" value="<?php echo @$respdata['cancel_url'];?>"/></td>
-			 	</tr>
-			 	<tr>
-					<td>Language	:</td><td><input type="text" name="language" value="<?php echo @$respdata['language'];?>"/></td>
-				</tr>
-		     	<tr>
-		     		<td colspan="2">Billing information(optional):</td>
-		     	</tr>
-		        <tr>
-		        	<td>Billing Name	:</td><td><input type="text" name="billing_name" value="<?php echo @$respdata['billing_name'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Billing Address	:</td><td><input type="text" name="billing_address" value="<?php echo @$respdata['billing_address'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Billing City	:</td><td><input type="text" name="billing_city" value="<?php echo @$respdata['billing_city'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Billing State	:</td><td><input type="text" name="billing_state" value="<?php echo @$respdata['billing_state'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Billing Zip	:</td><td><input type="text" name="billing_zip" value="<?php echo @$respdata['billing_zip'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Billing Country	:</td><td><input type="text" name="billing_country" value="<?php echo @$respdata['billing_country'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Billing Tel	:</td><td><input type="text" name="billing_tel" value="<?php echo @$respdata['billing_tel'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Billing Email	:</td><td><input type="text" name="billing_email" value="<?php echo @$respdata['billing_email'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td colspan="2">Shipping information(optional)</td>
-		        </tr>
-		        <tr>
-		        	<td>Shipping Name	:</td><td><input type="text" name="delivery_name" value="<?php echo @$respdata['delivery_name'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Shipping Address	:</td><td><input type="text" name="delivery_address" value="<?php echo @$respdata['delivery_address'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>shipping City	:</td><td><input type="text" name="delivery_city" value="<?php echo @$respdata['delivery_city'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>shipping State	:</td><td><input type="text" name="delivery_state" value="<?php echo @$respdata['delivery_state'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>shipping Zip	:</td><td><input type="text" name="delivery_zip" value="<?php echo @$respdata['delivery_zip'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>shipping Country	:</td><td><input type="text" name="delivery_country" value="<?php echo @$respdata['delivery_country'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Shipping Tel	:</td><td><input type="text" name="delivery_tel" value="<?php echo @$respdata['delivery_tel'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Merchant Param1	:</td><td><input type="text" name="merchant_param1" value="<?php echo @$respdata['merchant_param1'];?>"/></td>
-		        </tr>
-		        <tr>
-		        	<td>Merchant Param2	:</td><td><input type="text" name="merchant_param2" value="<?php echo @$respdata['merchant_param2'];?>"/></td>
-		        </tr>
-				<tr>
-					<td>Merchant Param3	:</td><td><input type="text" name="merchant_param3" value="<?php echo @$respdata['merchant_param3'];?>"/></td>
-				</tr>
-				<tr>
-					<td>Merchant Param4	:</td><td><input type="text" name="merchant_param4" value="<?php echo @$respdata['merchant_param4'];?>"/></td>
-				</tr>
-				<tr>
-					<td>Merchant Param5	:</td><td><input type="text" name="merchant_param5" value="<?php echo @$respdata['merchant_param5'];?>"/></td>
-				</tr>
-				<tr>
-					<td>Promo Code	:</td><td><input type="text" name="promo_code" value="<?php echo @$respdata['promo_code'];?>"/></td>
-				</tr>
-				<tr>
-					<td>Vault Info.	:</td><td><input type="text" name="customer_identifier" value="<?php echo @$respdata['customer_identifier'];?>"/></td>
-				</tr>
-		        <tr>
-		        	<td></td><td><INPUT TYPE="submit" value="CheckOut"></td>
-		        </tr>
-	      	</table>
-	      </form>
-	</body>
-	<script language='javascript'>document.customerData.submit();</script>
+<b>Please wait....</b>
+<form method="post" name="customerData" action="/cc/ccavRequestHandler.php">
+<input type="hidden" name="tid" id="tid" readonly /></td>
+<input type="hidden" name="merchant_id" value="<?php echo @$respdata['merchant_id'];?>"/></td>
+<input type="hidden" name="order_id" value="<?php echo @$respdata['order_id'];?>"/></td>
+<input type="hidden" name="amount" value="<?php echo @$respdata['amount'];?>"/></td>
+<input type="hidden" name="currency" value="<?php echo @$respdata['currency'];?>"/></td>
+<input type="hidden" name="redirect_url" value="<?php echo @$respdata['redirect_url'];?>"/></td>
+<input type="hidden" name="cancel_url" value="<?php echo @$respdata['cancel_url'];?>"/></td>
+<input type="hidden" name="language" value="<?php echo @$respdata['language'];?>"/></td>
+<input type="hidden" name="billing_name" value="<?php echo @$respdata['billing_name'];?>"/></td>
+<input type="hidden" name="billing_address" value="<?php echo @$respdata['billing_address'];?>"/></td>
+<input type="hidden" name="billing_city" value="<?php echo @$respdata['billing_city'];?>"/></td>
+<input type="hidden" name="billing_state" value="<?php echo @$respdata['billing_state'];?>"/></td>
+<input type="hidden" name="billing_zip" value="<?php echo @$respdata['billing_zip'];?>"/></td>
+<input type="hidden" name="billing_country" value="<?php echo @$respdata['billing_country'];?>"/></td>
+<input type="hidden" name="billing_tel" value="<?php echo @$respdata['billing_tel'];?>"/></td>
+<input type="hidden" name="billing_email" value="<?php echo @$respdata['billing_email'];?>"/></td>
+<input type="hidden" name="delivery_name" value="<?php echo @$respdata['delivery_name'];?>"/></td>
+<input type="hidden" name="delivery_address" value="<?php echo @$respdata['delivery_address'];?>"/></td>
+<input type="hidden" name="delivery_city" value="<?php echo @$respdata['delivery_city'];?>"/></td>
+<input type="hidden" name="delivery_state" value="<?php echo @$respdata['delivery_state'];?>"/></td>
+<input type="hidden" name="delivery_zip" value="<?php echo @$respdata['delivery_zip'];?>"/></td>
+<input type="hidden" name="delivery_country" value="<?php echo @$respdata['delivery_country'];?>"/></td>
+<input type="hidden" name="delivery_tel" value="<?php echo @$respdata['delivery_tel'];?>"/></td>
+<input type="hidden" name="merchant_param1" value="<?php echo @$respdata['merchant_param1'];?>"/></td>
+<input type="hidden" name="merchant_param2" value="<?php echo @$respdata['merchant_param2'];?>"/></td>
+<input type="hidden" name="merchant_param3" value="<?php echo @$respdata['merchant_param3'];?>"/></td>
+<input type="hidden" name="merchant_param4" value="<?php echo @$respdata['merchant_param4'];?>"/></td>
+<input type="hidden" name="merchant_param5" value="<?php echo @$respdata['merchant_param5'];?>"/></td>
+<input type="hidden" name="promo_code" value="<?php echo @$respdata['promo_code'];?>"/></td>
+<input type="hidden" name="customer_identifier" value="<?php echo @$respdata['customer_identifier'];?>"/>
+</form>
+</body>
+<script language='javascript'>document.customerData.submit();</script>
 </html>
